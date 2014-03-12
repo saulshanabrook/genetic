36caf8dd782070bc80188a99805b3d3e1b281f4b
Saul <s.shanabrook@gmail.com>
2014-03-11 21:55:30 -0400

    added eastwood linting support

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [roul "0.2.0"]]
-  :plugins [[lein-gorilla "0.1.2"]]
   :main ^:skip-aot genetic.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :test {:plugins [[jonase/eastwood "0.1.0"]]}})
